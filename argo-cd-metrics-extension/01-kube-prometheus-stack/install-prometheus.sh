fd04ea4bfc0bc2deca2091e62b889f50af2dd39f
Christian Knell <christian@knell.it>
2024-10-15 09:28:41 +0200

    aligning helm release names

--- argo-cd-metrics-extension/01-kube-prometheus-stack/install-prometheus.sh
+++ argo-cd-metrics-extension/01-kube-prometheus-stack/install-prometheus.sh
@@ -2,4 +2,4 @@
 
 helm dependency update
 
-helm upgrade --install prometheus --namespace monitoring --create-namespace -f values.yaml .
+helm upgrade --install kube-prometheus-stack --namespace monitoring --create-namespace -f values.yaml .
