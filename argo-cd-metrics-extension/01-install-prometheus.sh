8fdf6b0bc7c08e71f84228e64d48f155617eda60
Christian Knell <christian@knell.it>
2024-10-18 08:29:03 +0200

    fixed typo in wait flag

--- argo-cd-metrics-extension/01-install-prometheus.sh
+++ argo-cd-metrics-extension/01-install-prometheus.sh
@@ -4,6 +4,6 @@ cd 01-kube-prometheus-stack
 
 helm dependency update
 
-helm upgrade --install kube-prometheus-stack --namespace monitoring --create-namespace -f values.yaml -- wait .
+helm upgrade --install kube-prometheus-stack --namespace monitoring --create-namespace -f values.yaml --wait .
 
 cd ..
