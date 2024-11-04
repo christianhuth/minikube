1fc5a5a97225737825b430d67bb9ae4aeda7e5c8
Christian Knell <christian@knell.it>
2024-11-04 09:29:56 +0100

    add grafana dashboard

--- linux-user-and-group-monitoring-with-prometheus-textfile-collector/textfile-collector-scripts/linux-group-info.sh
+++ linux-user-and-group-monitoring-with-prometheus-textfile-collector/textfile-collector-scripts/linux-group-info.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 GROUPFILE="/etc/group"
 
 while IFS=':' read -r groupname password groupId users; do
-    echo "linux_group_info{groupname=\"$groupname\",gid=\"$groupId\",users=\"${users%,}\"} 1"
+    echo "linux_group_info{groupname=\"$groupname\",gid=\"$groupId\"} 1"
 done < "${GROUPFILE}"
